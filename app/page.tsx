d0e5fb4424c57b762198a4a04528598080e1e52d
robonexx <robertwagar@gmail.com>
2023-09-30 08:29:14 +0200

    arcticle cards, normal and small

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import HeroImages from './components/heroImages/HeroImages';
 import styles from './page.module.scss';
-import { getTopNews } from './lib/api';
+/* import { getTopNews } from './lib/api'; */
 
 
 const Home: React.FC = async () => {
 
-  const news = await getTopNews()
+/*   const news = await getTopNews()
 
-  console.log(news)
+  console.log(news) */
   return (
     <main className={styles.main}>
       <h1>NEWS NET</h1>
